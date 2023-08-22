0ea22a4741ad8b12a28045cf5452c2864a0af597
MarinaMali <marina.m@ku.th>
2023-08-22 07:15:00 +0700

    kanban_view2

--- resources/views/event/edit.blade.php
+++ resources/views/event/edit.blade.php
@@ -21,12 +21,27 @@
 <body>
 <h1 class="text-center text-blue-400 font-bold text-6xl uppercase mb-10">Kanban Bord</h1>
 
+<h2 class="text-center text-blue-400 font-bold text-2xl uppercase mb-10">To do</h2>
     <div class="bg-white p-10 rounded-lg shadow md:w-3/4 mx-auto lg:w-1/2">
-        Update About Event
+        Task 1 :
     </div>
 
+    <h2 class="text-center text-blue-400 font-bold text-2xl uppercase mb-10">Done</h2>
+    <div class="bg-white p-10 rounded-lg shadow md:w-3/4 mx-auto lg:w-1/2">
+        
+    </div>
+    
     <div class="mb-5">
-                    <label for="kanban" class="block mb-2 font-bold text-gray-600">Update Kanban</label>
+                    <label for="kanban" class="block mb-2 font-bold text-gray-600">To Do task:</label>
+                    <input type="text" id="kanban" name="kanban"
+                    required
+                    autocomplete = "on" placeholder="Put in Kanban" class="border border-gray-300 shadow p-3 w-full rounded mb-">
+                </div>
+
+    <button type="submit" class="block w-full bg-blue-500 text-white font-bold p-4 rounded-lg">create New Task</button>
+
+    <div class="mb-5">
+                    <label for="kanban" class="block mb-2 font-bold text-gray-600">Done task:</label>
                     <input type="text" id="kanban" name="kanban"
                     required
                     autocomplete = "on" placeholder="Put in Kanban" class="border border-gray-300 shadow p-3 w-full rounded mb-">
